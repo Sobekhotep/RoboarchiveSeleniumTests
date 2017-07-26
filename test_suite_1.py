9243c3358c718fbceaf472365996200bd258d529
Vlad Zankevich <vlad.zankevich@gmail.com>
2017-07-26 18:06:32 +0300

    Updated test suite

--- test_suite_1.py
+++ test_suite_1.py
@@ -1,9 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from unittest import TestCase
 
-driver = webdriver.Firefox()
-driver.get("http://roboarchive.org/search")
-assert "Robo Archive" in driver.title
-elem = driver.find_element_by_id("search-input")
-elem.send_keys("Уфа")
-elem.send_keys(Keys.RETURN)
+
+
+class MyFirstTest(TestCase):
+    def test_ufa(self):
+        driver = webdriver.Firefox()
+        driver.get("http://roboarchive.org/search")
+        assert "Robo Archive" in driver.title
+        elem = driver.find_element_by_id("search-input")
+        elem.send_keys("Уфа")
+        elem.send_keys(Keys.RETURN)
+
+    def test2(self):
+        pass
+
+    def test3(self):
+        pass
+    
