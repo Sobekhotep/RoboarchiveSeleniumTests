7c87863e99ffa67763a630c186a36dcf5dd07b54
Vlad Zankevich <vlad.zankevich@gmail.com>
2017-07-26 18:40:15 +0300

    Updated unittest

--- test_suite_1.py
+++ test_suite_1.py
@@ -1,3 +1,5 @@
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from unittest import TestCase
@@ -8,14 +10,18 @@ class MyFirstTest(TestCase):
     def test_ufa(self):
         driver = webdriver.Firefox()
         driver.get("http://roboarchive.org/search")
-        assert "Robo Archive" in driver.title
+        self.assertIn("Robo Archive", driver.title)
         elem = driver.find_element_by_id("search-input")
         elem.send_keys("Уфа")
         elem.send_keys(Keys.RETURN)
 
     def test2(self):
-        pass
+        asdf
 
     def test3(self):
-        pass
-    
+        x = 5
+        self.assertEqual(x, 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
