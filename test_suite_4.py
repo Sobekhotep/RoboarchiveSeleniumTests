4f0492d2abf7ac2ea979f6a3d927a9aba7619b59
Vlad Zankevich <vlad.zankevich@gmail.com>
2017-08-10 16:05:39 +0300

    Made test#1

--- test_suite_4.py
+++ test_suite_4.py
@@ -1,3 +1,5 @@
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
@@ -6,21 +8,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from unittest import TestCase
 
-class BaseSeleniumTestCase(TestCase):
+from test_suite_2 import BaseSeleniumTestCase
 
-    driver = webdriver.Firefox()
-    driver.get("http://roboarchive.org/search")
-
-    def wait_for_element(driver, locator, timeout=10, by=By.CSS_SELECTOR):
-      element = WebDriverWait(driver, timeout).until(
-          EC.presence_of_element_located((by, locator))
-      )
-      return element
 
 class TestURLField(BaseSeleniumTestCase):
 
     def test_url_after_click_search(self):
-        assert "roboarchive.org" in driver.current_url
+        search_url = "http://roboarchive.org/search"
+        self.driver.get(search_url)
+        url = self.driver.current_url
+
+        self.assertEqual(url, search_url)
+
 
 if __name__ == "__main__":
     unittest.main()
