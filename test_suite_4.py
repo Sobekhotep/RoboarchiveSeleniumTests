f85c482f540c39d1b2c53b248f15bc858d62ca0a
Vlad Zankevich <vlad.zankevich@gmail.com>
2017-08-10 18:33:13 +0300

    Added parse of URL

--- test_suite_4.py
+++ test_suite_4.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from unittest import TestCase
+from urllib.parse import urlparse
 
 from test_suite_2 import BaseSeleniumTestCase
 
@@ -25,7 +26,10 @@ class TestURLField(BaseSeleniumTestCase):
         url = "http://roboarchive.org/search?docdescription=false&doctitle=false&emptysearch=true&" \
               "fromdate=&gasrb=false&niab=false&page=1&search=&todate="
 
-        self.assertEqual(self.driver.current_url, url)
+        sample_url_parse = urlparse(url)
+        current_url_parse = urlparse(self.driver.current_url)
+
+        self.assertEqual(sample_url_parse, current_url_parse)
 
 
 if __name__ == "__main__":
